1467d38202d7bab9a790463ca1f676913b7e1585
Icaro Oliveira <icarovinici@gmail.com>
2021-02-25 19:05:47 -0300

    feat: add pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,98 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Button,
+  CardHeader,
+} from "@material-ui/core/";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import api from "@/src/config/api";
+
+type User = {
+  users: {
+    _id: string;
+    name: string;
+  }[];
+};
+
+type DataResponse = {
+  data: {
+    users: User[];
+  };
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      padding: theme.spacing(2),
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+  })
+);
+
+function Home({ users }: User) {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        {users.map((user) => (
+          <Grid item xs={12} sm={6} md={3} key={user._id}>
+            <Card>
+              <CardHeader title={`Name: ${user.name}`} />
+              <CardContent>
+                <Link href={`/${user._id}`}>
+                  <Button
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                  >
+                    View
+                  </Button>
+                </Link>
+                <Link href={`/${user._id}`}>
+                  <Button variant="contained" color="secondary">
+                    Edit
+                  </Button>
+                </Link>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get<DataResponse>("/users");
+  const { data } = response.data;
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+export default Home;
---
+++ src/pages/new.tsx
@@ -0,0 +1,107 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import {
+  Button,
+  CircularProgress,
+  Container,
+  TextField,
+} from "@material-ui/core";
+
+import api from "@/src/config/api";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      marginRight: theme.spacing(2),
+    },
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    form: {
+      display: "flex",
+    },
+  })
+);
+
+const NewUser = () => {
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState({
+    helperText: "",
+    error: false,
+  });
+
+  const router = useRouter();
+
+  const classes = useStyles();
+
+  const createUser = async () => {
+    try {
+      await api.post("/users", {
+        name: name,
+      });
+
+      router.push("/");
+      setIsLoading(false);
+      console.log("Success!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { value } = event.currentTarget;
+    setName(value);
+  };
+
+  const handlingError = () => {
+    if (name.length > 2) {
+      setErrors({ helperText: "", error: false });
+      setIsLoading(true);
+      createUser();
+    } else {
+      setErrors({ helperText: "Invalid name", error: true });
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handlingError();
+  };
+
+  return (
+    <Container className={classes.container}>
+      <h1>Create User</h1>
+      <div>
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField
+              id="outlined-required"
+              helperText={errors.helperText}
+              error={errors.error}
+              className={classes.margin}
+              onChange={handleChange}
+              label="Name"
+              placeholder="Enter your name"
+              variant="outlined"
+            />
+            <Button variant="contained" color="primary" type="submit">
+              Go!
+            </Button>
+          </form>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default NewUser;
