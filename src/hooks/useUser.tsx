171a5c35d6fc74ca46cf7f1ea95c404660b1cbe6
Icaro Oliveira <icarovinici@gmail.com>
2021-03-08 17:17:29 -0300

    feat: add custom hook

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,110 @@
+import { useRouter } from "next/router";
+import { createContext, useContext, useState } from "react";
+import api from "../config/api";
+
+type UserContextData = {
+  createUser: (name: string) => void;
+  updateUser: (id: string | string[], name: string) => void;
+  deleteUser: (id: string | string[]) => void;
+
+  isLoading: boolean;
+  setIsLoading: (value: React.SetStateAction<boolean>) => void;
+
+  handlingError: (name: string) => void;
+  errors: {
+    helperText: string;
+    error: boolean;
+  };
+};
+
+type UserContextProps = {
+  children: React.ReactNode;
+};
+
+export const UserContext = createContext<UserContextData>(
+  {} as UserContextData
+);
+
+export const UserContextProvider = ({ children }: UserContextProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState({
+    helperText: "",
+    error: false,
+  });
+
+  const router = useRouter();
+
+  const createUser = async (name: string) => {
+    try {
+      await api.post("/users", {
+        name: name,
+      });
+
+      router.push("/");
+      setIsLoading(false);
+      console.log("Created!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateUser = async (id: string, name: string) => {
+    try {
+      await api.put(`/users/${id}`, {
+        name: name,
+      });
+
+      router.push("/");
+      setIsLoading(false);
+      console.log("Updated!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteUser = async (id: string) => {
+    try {
+      await api.delete(`/users/${id}`);
+
+      console.log("User deleted!");
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handlingError = (name: string) => {
+    if (name.length > 2) {
+      setErrors({ helperText: "", error: false });
+      setIsLoading(true);
+    } else {
+      setErrors({ helperText: "Invalid name", error: true });
+    }
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        createUser,
+        updateUser,
+        deleteUser,
+        isLoading,
+        setIsLoading,
+        handlingError,
+        errors,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export function useUser() {
+  const context = useContext(UserContext);
+
+  if (!context) {
+    throw new Error("useUser must be used within a UserContextProvider");
+  }
+
+  return context;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,25 @@ import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import Layout from "@/src/components/Layout";
 import Navbar from "@/src/components/Navbar";
 import theme from "@/src/styles/theme";
+import { UserContextProvider } from "@/src/hooks/useUser";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Head>
-          <title>Home</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
-        <Navbar />
-        <CssBaseline />
-        <Component {...pageProps} />
-      </Layout>
+      <UserContextProvider>
+        <Layout>
+          <Head>
+            <title>Home</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+          </Head>
+          <Navbar />
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Layout>
+      </UserContextProvider>
     </ThemeProvider>
   );
 }
